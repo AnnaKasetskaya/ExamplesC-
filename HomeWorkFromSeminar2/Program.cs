0e35059be7413aa3eb3944f0773a0ace87c7a990
Anna Kasetskaya <anna.kasetskaya@gmail.com>
2022-08-12 15:35:31 +0300

    Исправлено решение в Задачи 13

--- HomeWorkFromSeminar2/Program.cs
+++ HomeWorkFromSeminar2/Program.cs
@@ -18,17 +18,22 @@
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 
-// Console.WriteLine("Введите число");
-// int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
 
-// if(num > -100 && num < 100)
-// {
-//     Console.WriteLine("Число меньше, чем трехзначное");
-// }
-// else
-// {
-//     Console.WriteLine((num % 1000 - num % 100) / 100);
-// }
+if(num > -100 && num < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while(Math.Abs(num) > 1000)
+    {
+        num = num / 10;
+    }
+    Console.WriteLine(Math.Abs(num) % 10);
+}
+    
 
 
 
