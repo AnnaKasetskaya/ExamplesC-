f9aea8b50530c2438aa4a30dae9f7da3d5670296
Anna Kasetskaya <anna.kasetskaya@gmail.com>
2022-08-21 21:21:39 +0300

    Убрала лишние строки в Example011_ArrayLibrary

--- Example011_ArrayLibrary/Program.cs
+++ Example011_ArrayLibrary/Program.cs
@@ -1,13 +1,13 @@
 ﻿void FillArray(int[] collection)
 {
-int length = collection.Length;
-int index = 0;
+    int length = collection.Length;
+    int index = 0;
 
-while(index < length)
-{
-    collection[index] = new Random().Next(1, 10);
-    index++;
-}
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
 }
 
 void PrintArray(int[] col)
